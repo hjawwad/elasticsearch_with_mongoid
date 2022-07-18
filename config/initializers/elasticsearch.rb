c124c11074d7a2f138012feb1689f07e0d51cd14
jawwad hussain <hjawwad456@gmail.com>
2022-07-18 08:23:10 +0500

    change elastic search query

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,4 +2,6 @@ require 'elasticsearch/model'
 
 # Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] || "http://localhost:9200
 client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || "http://localhost:9200"
+
+pp client.cluster.health
 # client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
